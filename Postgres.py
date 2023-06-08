dc69e82c23b567950059a32fb01b2b7e3ae01851
jirolk <sandrocastillo@unc.edu.py>
2023-06-07 21:37:07 -0400

    Ahora si ya hace la Backup

--- Postgres.py
+++ Postgres.py
@@ -20,7 +20,10 @@ dbname=os.getenv("pDATABASE_NAME")
 user = os.getenv("pDATABASE_USER")
 password = os.getenv("pDATABASE_PASSWORD")
 port = os.getenv("pDATABASE_PORT")
-         
+
+# Establecer la variable de entorno PGPASSWORD con la contraseña
+os.environ['PGPASSWORD'] = os.getenv('DATABASE_PASSWORD')
+
 #Log de las backup hechas
 logging.basicConfig(filename='log_Backup.txt', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
@@ -28,10 +31,10 @@ logging.basicConfig(filename='log_Backup.txt', level=logging.INFO, format='%(asc
 try:
     conn = psycopg2.connect(
         f"host={host} dbname={dbname} user={user} password={password} port={port}")
-    print('Conexión exitosa')
+    print('ConexiÃ³n exitosa')
 except (Exception, psycopg2.Error)  as error:
-    logging.error(f"Fallo con la conexión: {error}")
-    print(f'Falla con la conexión: {error}')
+    logging.error(f"Fallo con la conexiÃ³n: {error}")
+    print(f'Falla con la conexiÃ³n: {error}')
             
 # Crear un objeto cursor para ejecutar consultas SQL
 cursor = conn.cursor()
@@ -55,26 +58,40 @@ for db in db_list:
     archivo=os.path.join(carpeta, f"{db}.sql.gz")
     # comando para el respaldo de la BD postgres
     cmd = [
-        'pg_dump',
+        'pg_dump --no-privileges --no-owner --no-reconnect',
         '-h', os.getenv('pDATABASE_HOST'),
         '-U', os.getenv('pDATABASE_USER'),
-        # '-W', os.getenv('DATABASE_PASSWORD'),
+        #'-W', os.getenv('DATABASE_PASSWORD'),
         '-F', 'p',
         '-w', db,
         '|', 'gzip', '>', archivo
             ]
 
-    with subprocess.Popen(" ".join(cmd), stdin=subprocess.PIPE, shell=True) as proc:
-        proc.stdin.write(password.encode())
+    # subprocess.call(cmd,shell=True)
+    # subprocess.call(os.getenv('DATABASE_PASSWORD'), shell=True)
+    # with subprocess.Popen(" ".join(cmd), stdin=subprocess.PIPE, shell=True) as proc:
+    #     password = os.getenv('DATABASE_PASSWORD') + '\n'
+    #     proc.stdin.write(password.encode())
+    #     proc.communicate()
+    
+    with subprocess.Popen(" ".join(cmd), shell=True) as proc:
         proc.communicate()
+
+
+    
+    
+
+# Eliminar la variable de entorno PGPASSWORD después de su uso
+del os.environ['PGPASSWORD']
+
 
 print(f"\nBackup de la BD completado con exito en fecha: {datetime.now()}")
 logging.info(f"Postgres:Backup de la BD completado con exito. Cant:{len(db_list)}")
-# Cerrar el cursor y la conexión
+# Cerrar el cursor y la conexiÃ³n
 cursor.close()
 conn.close()
 
-#Fragmento para borrar los respaldos dependiendo de los días
+#Fragmento para borrar los respaldos dependiendo de los dÃ­as
 fecha_limite= datetime.now() - timedelta(days=dia)
 for nombreCarpeta in os.listdir(os.path.join(ruta, 'postgresSql')):
     
