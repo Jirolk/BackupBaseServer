1753adeac554573c249523f45760701125e25944
jirolk <sandrocastillo@unc.edu.py>
2023-06-13 14:33:09 -0400

    instaldor de instancia

---
+++ setupEntorno.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# exit when any command fails
+set -e
+
+directorio=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo Instalación del entorno para realizar la BACkUP
+echo $directorio
+cd $directorio
+
+echo Git pull
+git status
+git pull
+
+if [ -d "env" ] 
+then
+    echo "Entorno virtual existente" 
+else
+	echo "Creamos un nuevo entorno virtual" 
+    python3.6 -m virtualenv env
+fi
+echo Install Requirements
+source env/bin/activate
+pip install -r requirements.txt
+
+#agregamos la tarea al crontab
+(crontab -l 2>/dev/null; echo "0 1 * * * bash $directorio/backup.sh") | crontab -
+
+echo Fin
